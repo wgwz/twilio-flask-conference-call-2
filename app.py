1ed3aa640e20f773a0bf8fa75951234c2ad57576
Kyle Lawlor-Bagcal <wgwz@pm.me>
2021-07-24 13:06:43 -0400

    who's speaking?

--- app.py
+++ app.py
@@ -9,19 +9,25 @@ logger = logging.getLogger()
 app = Flask(__name__)
 app.config.update({"SECRET_KEY": "mysecret"})
 socketio = SocketIO(app)
+caller_cache = {}
 
 
 @app.route("/conference", methods=["POST"])
 def conference():
-    print(f"{request.form=}")
+    print(f"/conference: {request.form=}")
+    print(f"/conference: {caller_cache=}")
     response = VoiceResponse()
     pin_code = request.form.get("Digits")
+    call_sid = request.form.get("CallSid")
+    caller = request.form.get("Caller")
+    if call_sid and caller:
+        caller_cache[call_sid] = caller
     if pin_code and pin_code == "12345":
         dial = Dial()
         dial.conference(
             "Twilio Test Conference",
             status_callback="/events",
-            status_callback_event="start end join leave mute hold",
+            status_callback_event="start end join leave mute hold speaker",
         )
         response.append(dial)
     else:
@@ -33,7 +39,14 @@ def conference():
 
 @app.route("/events", methods=["POST"])
 def events():
-    socketio.emit("conference event", dict(request.form))
+    print(f"/events: {request.form=}")
+    print(f"/events: {caller_cache=}")
+    data = dict(request.form)
+    call_sid = data.get("CallSid")
+    caller = caller_cache.get(call_sid)
+    if call_sid and caller:
+        data["Caller"] = caller
+    socketio.emit("conference event", data)
     return "", 200
 
 
